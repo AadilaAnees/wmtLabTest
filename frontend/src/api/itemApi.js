cc9d1adec9353e90ccf9a1dbb3d5f5d14f0bdb06
Aadhila M.A.F IT24102420 <it24102420@my.sliit.lk>
2026-04-25 20:00:38 +0530

    updated env vrible

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://wmtlabtest-6ydh.onrender.com/api/items",
+  baseURL: import.meta.env.VITE_API_URL || "https://wmtlabtest-6ydh.onrender.com/api",
 });
 
 export const getItems = () => API.get("/items");
